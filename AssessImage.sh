3df08294bda80c78e7f981fe8540bd4e07171327
Chandan Dwivedi <chhandan.dwivedi99@gmail.com>
2019-11-13 13:42:26 +0530

    Fixed AssessImage.sh
    did the changes required to remove the error from the script and make it able to run on all images of the directory.

--- AssessImage.sh
+++ AssessImage.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-if [[ $# -eq 1 ]]; then 
-./featur $1; 
-./res theta.txt feature.txt; 
+if [[ $# -eq 1 ]]; then
+echo "$1";
+for files in "$1"/*
+do
+  echo "$files"
+  ./featur "$files"
+  ./res theta.txt feature.txt
+done
 fi
-
